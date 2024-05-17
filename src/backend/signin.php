adcfa6ced0e0ec7083dcf58d3ec544d64bc90145
anderson <ander02guev@gmail.com>
2024-05-17 12:10:42 -0500

    3corte

--- src/backend/signin.php
+++ src/backend/signin.php
@@ -1,26 +1,40 @@
 <?php
-   include('../../config/database.php');
-   $email =$_POST['email'];
-   $passwd =$_POST['password'];
-   $enc_pass = md5($passwd);
+    include('../../config/database.php');
 
-   $sql="
-      select
-         *
-      from 
-         users 
-      where
-         email = '$email' And
-         password = '$enc_pass'
-      limit 1
-   ";
-   
-   $result = pg_query($conn,$sql);
-   $total = pg_num_rows( $result);
+    session_start();
 
-   if($total > 0){
-      header("refresh:0;url=../home.php");
-   }else{
-      echo "credenciales incorrectas";
-   }
+    if(isset($_SESSION["id_user"])){
+        header("Location:../home.php");
+    }
+
+    
+    if(!empty($_POST)){
+        $email = $_POST['email'];
+        $passwd = $_POST['passwd'];
+        $enc_pass = md5($passwd);
+
+        $sql = "
+            SELECT
+                *
+            FROM
+                users
+            WHERE
+                email = '$email' AND
+                password = '$enc_pass'
+            LIMIT 1
+        ";
+
+        $result = pg_query($conn, $sql);
+        $total = pg_num_rows($result);
+
+        if ($total > 0){
+            $row = pg_fetch_assoc($result);
+            $_SESSION['id_user'] = $row['id'];
+            $_SESSION['user_name'] = $row['fullname'];
+            header("refresh:0;url=../home.php");
+        }else{
+            echo "<script>alert('Invalid email or password')</script>";
+            header("refresh:0;url=../signin.php");
+        }   
+    }
 ?>
